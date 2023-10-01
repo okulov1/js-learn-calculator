cca73a39a833d1cf41ae687181ea36bb3ad7bc1f
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-10-01 19:19:03 +0300

    calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+let result_button = document.getElementById('result-button'),
+    first_num = document.getElementById('first_number'),
+    last_num = document.getElementById('last_number'),
+    action = document.getElementById('action');
+
+result_button.addEventListener('click', get_result);
+
+function get_result(){
+    let fn = parseFloat(first_num.value),
+        ln = parseFloat(last_num.value),
+        a = action.value;
+    
+    let result;
+
+    if (a == 'plus') {
+        result = fn + ln;
+    } else if (a == 'minus') {
+        result = fn - ln;
+    } else if (a == 'multiply') {
+        result = fn * ln;
+    } else if (a == 'divide') {
+        result = fn / ln;
+    }
+
+    document.getElementById('show_result').innerHTML = result;
+}
